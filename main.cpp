2ce7d86b2cd5220d296b72d3d79ccb1f23ec268d
Raoof Alaa <120230697+RaoofAlaa@users.noreply.github.com>
2025-04-24 11:00:18 +0200

    Update main.cpp
    -I added comments to make the code more readable and declarative.
    -I added prompts to make it easier for the user!

--- main.cpp
+++ main.cpp
@@ -3,12 +3,16 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(false);
-
+    //Variable to store the card's number
     string card_number;
+    cout<<"Hello!\nEnter your card number to check if it's valid or not!\n";
     cin>>card_number;
 
-
+    //Sum that will be checked later if it's divisible by zero
     int sum=0;
+
+    //Looping on every second digit, multiplying and adding digits if product is greater than 9
+    //Also added to the sum in the same loop
     for(int i=0;i<card_number.size();i+=2)
     {
         if((card_number[i]-'0')*2>9)
@@ -20,19 +24,21 @@ int main()
             sum+=(card_number[i]-'0')*2;
         }
     }
-
+    //Summing odd positions as they are
     for(int i=1;i<card_number.size();i+=2)
     {
         sum+=card_number[i]-'0';
     }
 
+    //Checking the validity of the card
+    cout<<"Sum equals = "<<sum<<endl;
     if(sum%10==0)
     {
         cout<<"Valid Card Number";
     }
     else
     {
-        cout<<"Not a Valid Card Number";
+        cout<<"Invalid Card Number";
     }
 
 
